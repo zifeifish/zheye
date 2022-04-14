888feff0964c549442ad98e93118399cd7f3f25b
zifeifish <51848087+zifeifish@users.noreply.github.com>
2022-04-14 23:10:29 +0800

    点击外部区域隐藏dropdown

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,16 +1,16 @@
 <template>
-<div class="dropdown">
-  <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
-    {{title}}
-  </a>
-  <ul class="dropdown-menu" :style="{display: 'block'}" v-if="isOpen">
-    <slot></slot>
-  </ul>
-</div>
+  <div class="dropdown" ref="dropdownRef">
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">
+      {{ title }}
+    </a>
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <slot></slot>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -21,12 +21,26 @@ export default defineComponent({
   },
   setup () {
     const isOpen = ref(false)
+    const dropdownRef = ref<HTMLElement | null>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const dropdownHandler = (e: MouseEvent) => {
+      // e.target as HTMLElement 类型断言防止报错
+      if (!dropdownRef.value?.contains(e.target as HTMLElement) && isOpen.value) {
+        isOpen.value = false
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', dropdownHandler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', dropdownHandler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef // 返回的变量名与绑定的ref名称一致
     }
   }
 })
