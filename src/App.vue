3e1183da47007c74d35a20f57227eff71ee32678
zifeifish <51848087+zifeifish@users.noreply.github.com>
2022-04-14 21:32:22 +0800

    GlobalHeader组件编码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="user"></global-header>
     <column-List :list="list"></column-List>
   </div>
 </template>
@@ -8,6 +9,7 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
 const testData: ColumnProps[] = [
   {
@@ -21,17 +23,48 @@ const testData: ColumnProps[] = [
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test3的专栏',
+    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test4的专栏',
+    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test5的专栏',
+    description: '这是的test5专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test6的专栏',
+    description: '这是的test6专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
+
+const userInfo: UserProps = {
+  isLogin: false,
+  name: 'hcy'
+}
 
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      user: userInfo
     }
   }
 })
