276f8f3236273b1c29332510dccdb3b57ed7a89d
zifeifish <51848087+zifeifish@users.noreply.github.com>
2022-04-18 22:50:31 +0800

    ValidtateInput抽象验证规则

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,55 @@
   <div class="container">
     <global-header :user="user"></global-header>
     <simply-form></simply-form>
-    <column-List :list="list"></column-List>
+    <!-- <column-List :list="list"></column-List> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import SimplyForm from './components/SimplyForm.vue'
+import SimplyForm from './components/Form/SimplyForm.vue'
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test5的专栏',
-    description: '这是的test5专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test6的专栏',
-    description: '这是的test6专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test3的专栏',
+//     description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test4的专栏',
+//     description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test5的专栏',
+//     description: '这是的test5专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test6的专栏',
+//     description: '这是的test6专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   }
+// ]
 
 const userInfo: UserProps = {
   isLogin: true,
@@ -60,13 +60,13 @@ const userInfo: UserProps = {
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
+    // ColumnList,
     GlobalHeader,
     SimplyForm
   },
   setup () {
     return {
-      list: testData,
+      // list: testData,
       user: userInfo
     }
   }
---
+++ src/components/Form/SimplyForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form action="">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+      <validtate-input :rules="emailRules"></validtate-input>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">密码</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" />
+    </div>
+  </form>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import ValidtateInput, { RulesProp } from './ValidtateInput.vue'
+
+export default defineComponent({
+  name: 'SimplyForm',
+  components: {
+    ValidtateInput
+  },
+  setup () {
+    const emailRules = reactive<RulesProp>([
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ])
+
+    return {
+      emailRules
+    }
+  }
+})
+</script>
+<style>
+</style>
