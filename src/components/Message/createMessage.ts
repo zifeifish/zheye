ca89244f24f300879b54f4c0b90c383a8550f5cd
zifeifish <51848087+zifeifish@users.noreply.github.com>
2022-05-05 22:52:18 +0800

    h函数改造message

--- src/components/Message/createMessage.ts
+++ src/components/Message/createMessage.ts
@@ -1,16 +1,17 @@
-import { createApp } from 'vue'
+import { h, render } from 'vue'
 import Message from './Message.vue'
 export type MessageType = 'success' | 'error' | 'default';
 const createMessage = (message: string, type: MessageType, timeout = 2000) => {
-  const messageInstance = createApp(Message, {
+  const messageVnode = h(Message, {
     message,
     type
   })
   const mountNode = document.createElement('div')
   document.body.appendChild(mountNode)
-  messageInstance.mount(mountNode)
+  render(messageVnode, mountNode)
   setTimeout(() => {
-    messageInstance.unmount()
+    render(null, mountNode)
+    document.body.removeChild(mountNode)
   }, timeout)
 }
 export default createMessage
